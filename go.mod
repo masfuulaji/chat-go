9493c49cb37da68d72290295076f65e982c32e00
masfuulaji <masfuulaji@gmail.com>
2023-08-22 18:49:21 +0700

    add cors

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/masfuulaji/go-chat
 
 go 1.20
 
-require github.com/gorilla/websocket v1.5.0 // indirect
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/rs/cors v1.9.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,35 +5,45 @@ import (
 	"net/http"
 
 	"github.com/masfuulaji/go-chat/pkg/websocket"
+	"github.com/rs/cors"
 )
 
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
 func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
-   fmt.Println("websocket enpoint reached") 
+	fmt.Println("websocket enpoint reached")
 
-   conn, err := websocket.Upgrade(w, r)
-   if err != nil {
-      fmt.Fprintf(w, "%+v\n", err)
-   }
+	conn, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+	}
 
-   client := &websocket.Client{
-      Conn: conn,
-      Pool: pool,
-   }
-   pool.Register <- client
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+	pool.Register <- client
 
-   client.Read()
+	client.Read()
 }
 
-func setupRoute() {
-    pool := websocket.NewPool() 
-    go pool.Start()
+func setupRoute(mux *http.ServeMux) {
+	pool := websocket.NewPool()
+	go pool.Start()
 
-    http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-       serveWs(pool, w, r) 
-    })
+    mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
 }
+
 func main() {
-    fmt.Println("Hello, World!")
-    setupRoute()
-    http.ListenAndServe("9000", nil)
+	mux := http.NewServeMux()
+    mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+	setupRoute(mux)
+    handler := cors.Default().Handler(mux)
+	http.ListenAndServe(":8080", handler)
 }
